f97be8ef4be287d0584ade9021ac165d43426d69
ssrang1228 <163647116+ssrang1228@users.noreply.github.com>
2024-03-23 19:44:55 +0900

    Update 0702학습.py

--- 0702학습.py
+++ 0702학습.py
@@ -1,2 +1,5 @@
+git remote add upstream [https://github.com/Yebin0319/test-repo2/blob/main/0702%ED%95%99%EC%8A%B5.py]
+git fetch upstream
+
 from functools import reduce
 print(reduce(lambda x,y: x*y,[1,2,3,4,5]))
